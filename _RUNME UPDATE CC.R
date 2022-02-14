79750e4f398a1ec9a05be6e1093925d6fafd49a8
sihongliu0210 <sihongliu.0210@gmail.com>
2022-02-14 12:16:12 -0800

    Fix mismatch issues in the CC questions

--- _RUNME UPDATE CC.R
+++ _RUNME UPDATE CC.R
@@ -70,7 +70,7 @@ ec_response_table = ec_master %>%
 
 
 ec_questions = ec_master %>% 
-    select(OPEN.001, OPEN.002, OPEN.003, OPEN.004, OPEN.005, OPEN.007, OPEN.008, OPEN.009, OPEN.010,
+    select(OPEN.001, OPEN.002, OPEN.003, OPEN.004, OPEN.005, OPEN.007, OPEN.006, OPEN.008, OPEN.009, OPEN.010,
            COVID.007, COVID.008, CTAX.008, CTAX.016, DEBT.005, DEBT.006, FamCon.016, FamCon.017, FamCon.018, GRAND.016, HEALTH.024, 
            HEALTH.030, JOB.030, MH.013, MH.014, POLICY.030, PREG.028, PREG.029, PREG.038, PREG.040, PREG.044, STIM.001.d, STIM.002.d, STIM.003.d, STIM.004.d, STIM.005.d,  WKFORCE.011,
            OPEN.011, POLICY.033, NEEDS.007, WIC.009, CTAX.022) %>% 
@@ -93,8 +93,9 @@ cc_response_table = cc_master %>%
                                        CC.DEMO.013_6 == 1|CC.DEMO.013_10 == 1 | CC.DEMO.013_11 == 1 ~ "Babysitter/nanny"), 
             Language = case_when (UserLanguage == "EN" ~ "English", 
                                   UserLanguage == "SPA" ~ "Spanish")) %>%
-    select(ProviderID, Week, starts_with("CC.OPEN"), CC.CCS.004, CC.CCS.005, CC.CTAX.008, CC.CTAX.016, CC.DEBT.005, CC.DEBT.006, CC.FamCon.016, CC.FamCon.017, CC.FamCon.018, CC.MH.013, CC.SF.010, CC.Staff.009, 
-           CC.STIM.001.d, CC.STIM.002.d, CC.STIM.003.d, CC.STIM.004.d, CC.STIM.005.d, CC.WellB.002, CC.VACC.003, CC.VACC.003.1, 
+    select(ProviderID, Week, CC.OPEN.001, CC.OPEN.002, CC.OPEN.003, CC.OPEN.004, CC.OPEN.005, CC.OPEN.006, CC.OPEN.007, CC.OPEN.008, 
+           CC.CCS.004, CC.CCS.005, CC.CTAX.008, CC.CTAX.016, CC.DEBT.005, CC.DEBT.006, CC.FamCon.016, CC.FamCon.017, CC.FamCon.018, CC.MH.013, CC.SF.010, CC.Staff.009, 
+           CC.STIM.001.d, CC.STIM.002.d, CC.STIM.003.d, CC.STIM.004.d, CC.STIM.005.d, CC.WellB.002, CC.VACC.003, CC.VACC.003.1, CC.OPEN.009, CC.OPEN.010, CC.OPEN.011,
            State, FPL.2019.150, zip, RaceGroup, provider_type, Language) %>%
     filter(CC.OPEN.007 == 1) %>%
     arrange(Week) %>%
@@ -104,19 +105,23 @@ cc_response_table = cc_master %>%
     ungroup() %>%
     select(-CC.OPEN.007) %>%
     rename(`Below 1.5xFPL` = FPL.2019.150) %>%
-    gather("Question", "Response", starts_with("CC.OPEN"), CC.CCS.004, CC.CCS.005, CC.CTAX.008, CC.CTAX.016, CC.DEBT.005, CC.DEBT.006, CC.FamCon.016, CC.FamCon.017, CC.FamCon.018, CC.MH.013, CC.SF.010, CC.Staff.009, 
-           CC.STIM.001.d, CC.STIM.002.d, CC.STIM.003.d, CC.STIM.004.d, CC.STIM.005.d, CC.WellB.002, CC.VACC.003, CC.VACC.003.1) %>%
+    gather("Question", "Response", CC.OPEN.001, CC.OPEN.002, CC.OPEN.003, CC.OPEN.004, CC.OPEN.005, CC.OPEN.006, CC.OPEN.008, 
+           CC.CCS.004, CC.CCS.005, CC.CTAX.008, CC.CTAX.016, CC.DEBT.005, CC.DEBT.006, CC.FamCon.016, CC.FamCon.017, CC.FamCon.018, CC.MH.013, CC.SF.010, CC.Staff.009, 
+           CC.STIM.001.d, CC.STIM.002.d, CC.STIM.003.d, CC.STIM.004.d, CC.STIM.005.d, CC.WellB.002, CC.VACC.003, CC.VACC.003.1,
+           CC.OPEN.009, CC.OPEN.010, CC.OPEN.011) %>%
     filter(Response != "") 
 
 
 cc_questions = cc_master %>% 
-    select(starts_with("CC.OPEN"), CC.CCS.004, CC.CCS.005, CC.CTAX.008, CC.CTAX.016, CC.DEBT.005, CC.DEBT.006, CC.FamCon.016, CC.FamCon.017, CC.FamCon.018, CC.MH.013, CC.SF.010, CC.Staff.009, 
-           CC.STIM.001.d, CC.STIM.002.d, CC.STIM.003.d, CC.STIM.004.d, CC.STIM.005.d, CC.WellB.002, CC.VACC.003, CC.VACC.003.1) %>% 
+    select(CC.OPEN.001, CC.OPEN.002, CC.OPEN.003, CC.OPEN.004, CC.OPEN.005, CC.OPEN.006, CC.OPEN.007,CC.OPEN.008, 
+           CC.CCS.004, CC.CCS.005, CC.CTAX.008, CC.CTAX.016, CC.DEBT.005, CC.DEBT.006, CC.FamCon.016, CC.FamCon.017, CC.FamCon.018, CC.MH.013, CC.SF.010, CC.Staff.009, 
+           CC.STIM.001.d, CC.STIM.002.d, CC.STIM.003.d, CC.STIM.004.d, CC.STIM.005.d, CC.WellB.002, CC.VACC.003, CC.VACC.003.1,
+           CC.OPEN.009, CC.OPEN.010, CC.OPEN.011) %>% 
     select(-CC.OPEN.007)
 cc_q_text = sjlabelled::get_label(cc_questions)
 cc_q_names = names(cc_questions)
 #q_nums = as.numeric(str_remove(q_names, "OPEN."))
-cc_q_nums = seq(1, 27)
+cc_q_nums = seq(1, 30)
 #rm(questions)
 
 # loop bigram  ------------------------------------------------------------
